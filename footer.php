a73c6f08052638d5e2f1cc7c50c7f0c4e1f6264c
Sumit Kalyana <54006862+Sumitsrk25@users.noreply.github.com>
2024-03-05 22:39:52 +0530

    First Commit

---
+++ footer.php
@@ -0,0 +1,110 @@
+<footer class="pt-5 bg-light">
+	<div class="container">
+		<!-- Row START -->
+		<div class="row g-4">
+
+			<!-- Widget 1 START -->
+			<div class="col-lg-3">
+				<!-- logo -->
+				<a class="me-0" href="index.html">
+					<img class="light-mode-item " src="decocon_logo.png" alt="logo">
+					<img class="dark-mode-item " src="decocon_logo.png" alt="logo">
+				</a>
+				<p class="my-3">At our Decorative Flooring Services, we know that it's important for your floors to look good and work well. We believe that your floors should be pretty & strong & easy to take care of. Make your home or office any other space look better with us. We have numerous types of flooring to choose from that match your style and what you like. </p>
+				<!-- Social media icon -->
+				<ul class="list-inline mb-0 mt-3">
+					<li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-facebook" href="https://www.facebook.com/profile.php?id=100091577787281"><i class="fab fa-fw fa-facebook-f"></i></a> </li>
+					<li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-youtube" href="https://www.youtube.com/channel/UCL93WNcxy2ndgUl6yAin7GA"><i class="fab fa-fw fa-youtube"></i></a> </li>
+				
+					<li class="list-inline-item"> <a class="btn btn-white btn-sm shadow px-2 text-linkedin" href="https://www.linkedin.com/company/decocon-pvt-ltd/?viewAsMember=true"><i class="fab fa-fw fa-linkedin-in"></i></a> </li>
+				</ul>
+			</div>
+			<!-- Widget 1 END -->
+
+			<!-- Widget 2 START -->
+			<div class="col-lg-7">
+				<div class="row g-4">
+					<!-- Link block -->
+					<div class="col-6 col-md-4">
+						<h5 class="mb-2 mb-md-4">Company</h5>
+						<ul class="nav flex-column">
+						    <li class="nav-item"><a class="nav-link" href="http://decocon.in/">Home</a></li>
+							<li class="nav-item"><a class="nav-link" href="http://decocon.in/about.php">About us</a></li>
+							<li class="nav-item"><a class="nav-link" href="http://decocon.in/contact.php">Contact us</a></li>
+
+							<li class="nav-item"><a class="nav-link" href="http://decocon.in/faq.php">FAQ's</a></li>
+						
+						</ul>
+					</div>
+									
+					 
+				
+
+					<!-- Link block -->
+					<div class="col-6 col-md-4">
+						<h5 class="mb-2 mb-md-4">Products</h5>
+						<ul class="nav flex-column">
+							<li class="nav-item"><a class="nav-link" href="product/bespoke-terrazzo.php">Bespoke In-Situ Terrazzo </a></li>
+							<li class="nav-item"><a class="nav-link" href="product/cementitious-terrazzo-cast-in-situ-flooring.php">Terrazzo In-Situ - FANE’IA</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/terrazzo-slab.php">Terrazzo Slabs - KWAD’RA</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/terrazzo-tiles.php">Terrazzo Tiles- TERR’AV</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/epoxy-terrazzo-cast-in-situ-flooring.php">Epoxy Terrazzo - FORM’AX</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/microtopping-concrete-flooring.php">Micro Topping</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/polished-concrete-screed.php">Polishable Screed - FLUS’SO</a></li>
+							<li class="nav-item"><a class="nav-link" href="product/polyurethane-terrazzo.php">PU Concrete - INIC’IO</a></li>
+						
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- Widget 2 END -->
+
+			<!-- Widget 3 START -->
+			<div class="col-lg-2">
+				<h5 class="mb-2 mb-md-4">Contact</h5>
+				<!-- Time -->
+				<p class="mb-2">
+					Toll free:<span class="h6 fw-light ms-2">+91-9175942862</span>
+					<span class="d-block small">(9:AM to 8:PM IST)</span>
+				</p>
+
+				<p class="mb-0">Email:<span class="h6 fw-light ms-2">sales@decocon.in</span></p>
+
+
+<br/>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15132.077754116486!2d73.8407806!3d18.5280237!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf8dcb324b03%3A0x1627a11b4d4cf162!2sDecocon%20Terrazzo%20Tiles%20%26%20Flooring!5e0!3m2!1sen!2sin!4v1692211295486!5m2!1sen!2sin" width="300" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+
+
+
+				
+			</div> 
+			<!-- Widget 3 END -->
+		</div><!-- Row END -->
+
+		<!-- Divider -->
+		<hr class="mt-4 mb-0">
+
+		<!-- Bottom footer -->
+		<div class="py-3">
+			<div class="container px-0">
+				<div class="d-md-flex justify-content-between align-items-center py-3 text-center text-md-left">
+					<!-- copyright text -->
+					<div class="text-primary-hover"> Copyrights <a href="#" class="text-body">©2023 Decocon</a>. All rights reserved. </div>
+					<!-- copyright links-->
+					<div class=" mt-3 mt-md-0">
+						<ul class="list-inline mb-0">
+							
+								<li class="list-inline-item"><a class="nav-link" href="#">Sitemap</a></li>
+							<li class="list-inline-item"><a class="nav-link" href="#">Terms of use</a></li>
+							<li class="list-inline-item"><a class="nav-link pe-0" href="#">Privacy policy</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ header1.php
@@ -0,0 +1,90 @@
+   <?php 
+$pg = basename(substr($_SERVER['PHP_SELF'],0,strrpos($_SERVER['PHP_SELF'],'.'))); // get file name from url and strip extension
+?>
+    <!-- Header START -->
+    <header class="navbar-light navbar-sticky">
+        <!-- Logo Nav START -->
+         
+
+        <nav class="navbar navbar-expand-xl">
+            <div class="container">
+                <!-- Logo START -->
+                <a class="navbar-brand" href=""
+                    style="padding-top: 10px !important;padding-bottom: 10px !important;">
+                    <img class="light-mode-item navbar-brand-item" style="width:300px;height:100px" src="../decocon_logo.png"
+                        style="width: 150px;height:50px" alt="logo" />
+                    <img class="dark-mode-item navbar-brand-item" src="assets/images/logo-light.svg" alt="logo" />
+                </a>
+                <!-- Logo END -->
+
+                <!-- Responsive navbar toggler -->
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-animation">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
+                </button>
+
+<style>
+.active1{
+    color:white !important;
+    background-color: #3168b2;
+}
+</style>
+                <!-- Main navbar START -->
+                <div class="navbar-collapse w-100 collapse" id="navbarCollapse">
+                    <!-- Nav Main menu START -->
+      <ul class="navbar-nav navbar-nav-scroll mx-auto">
+
+                        <li class="nav-item mx-2">
+                            <a class="nav-link headerfont   <?php if($pg=='index'){?>active1<?php }?>" href="../index.php" >HOME</a>
+                        </li>
+
+                        
+                        <li class="nav-item mx-2">
+                            <a class="nav-link headerfont <?php if($pg=='about'){?>active1<?php }?>" " href="../about.php"  >ABOUT</a>
+                        </li>
+                        
+                        
+                     
+                     
+<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle headerfont active1"  href="#" id="demoMenu" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >PRODUCTS</a>
+						<ul class="dropdown-menu" aria-labelledby="demoMenu">
+							<li> <a class="dropdown-item" href="http://decocon.in/product/bespoke-terrazzo.php">Bespoke In-Situ Terrazzo Flooring</a></li>
+							<li> <a class="dropdown-item " href="http://decocon.in/product/cementitious-terrazzo-cast-in-situ-flooring.php">Cementitious Terrazzo In-Situ System- FANE’IA</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/terrazzo-slab.php">Cementitious Terrazzo Slabs- KWAD’RA</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/terrazzo-tiles.php">Cementitious Terrazzo Tiles- TERR’AV</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/epoxy-terrazzo-cast-in-situ-flooring.php">Epoxy Terrazzo In-Situ system- FORM’AX</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/microtopping-concrete-flooring.php">Micro Topping</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/polished-concrete-screed.php">Polishable Screed In-Situ Flow-ps- FLUS’SO</a></li>
+							<li> <a class="dropdown-item" href="http://decocon.in/product/polyurethane-terrazzo.php">PU Concrete In-Situ System- INIC’IO</a></li>
+						
+						
+						</ul>
+					</li>
+
+                    <li class="nav-item mx-2">
+                            <a class="nav-link headerfont <?php if($pg=='faq'){?>active1<?php }?>" " href="../faq.php" >FAQ's</a>
+                        </li>
+
+                        <!-- Nav item 4 Component-->
+                        <li class="nav-item mx-2">
+                            <a class="nav-link headerfont <?php if($pg=='contact'){?>active1<?php }?>" " href="../contact.php">CONTACT</a>
+                        </li>
+                    </ul>
+                    <!-- Nav Main menu END -->
+
+
+                </div>
+                <!-- Main navbar END -->
+
+
+            </div>
+        </nav>
+        <!-- Logo Nav END -->
+    </header>
+    <!-- Header END -->
